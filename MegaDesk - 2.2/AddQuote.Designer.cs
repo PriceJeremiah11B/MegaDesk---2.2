2815a4a19878371160bbda9e307952405ff00122
PriceJeremiah11B <52722550+PriceJeremiah11B@users.noreply.github.com>
2020-05-09 13:00:46 -0600

    Add project files.

---
+++ MegaDesk - 2.2/AddQuote.Designer.cs
@@ -0,0 +1,270 @@
+﻿using System;
+
+namespace MegaDesk___2._2
+{
+    partial class AddQuote
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.customerNameBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.sevenDayRadio = new System.Windows.Forms.RadioButton();
+            this.rushButton = new System.Windows.Forms.Button();
+            this.fiveDayRadio = new System.Windows.Forms.RadioButton();
+            this.threeDayRadio = new System.Windows.Forms.RadioButton();
+            this.normalRadio = new System.Windows.Forms.RadioButton();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CreateQuote = new System.Windows.Forms.Button();
+            this.drawerNumberBox = new System.Windows.Forms.TextBox();
+            this.DepthBox = new System.Windows.Forms.TextBox();
+            this.WidthBox = new System.Windows.Forms.TextBox();
+            this.MaterialCombo = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // customerNameBox
+            // 
+            this.customerNameBox.Location = new System.Drawing.Point(340, 42);
+            this.customerNameBox.Name = "customerNameBox";
+            this.customerNameBox.Size = new System.Drawing.Size(121, 20);
+            this.customerNameBox.TabIndex = 35;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(200, 49);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 13);
+            this.label6.TabIndex = 34;
+            this.label6.Text = "Customer Name";
+            // 
+            // sevenDayRadio
+            // 
+            this.sevenDayRadio.AutoSize = true;
+            this.sevenDayRadio.Location = new System.Drawing.Point(340, 301);
+            this.sevenDayRadio.Name = "sevenDayRadio";
+            this.sevenDayRadio.Size = new System.Drawing.Size(58, 17);
+            this.sevenDayRadio.TabIndex = 33;
+            this.sevenDayRadio.TabStop = true;
+            this.sevenDayRadio.Text = "7 Days";
+            this.sevenDayRadio.UseVisualStyleBackColor = true;
+            this.sevenDayRadio.Visible = false;
+            // 
+            // rushButton
+            // 
+            this.rushButton.Location = new System.Drawing.Point(512, 234);
+            this.rushButton.Name = "rushButton";
+            this.rushButton.Size = new System.Drawing.Size(88, 34);
+            this.rushButton.TabIndex = 32;
+            this.rushButton.Text = "Show Rush Options";
+            this.rushButton.UseVisualStyleBackColor = true;
+            this.rushButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // fiveDayRadio
+            // 
+            this.fiveDayRadio.AutoSize = true;
+            this.fiveDayRadio.Location = new System.Drawing.Point(340, 278);
+            this.fiveDayRadio.Name = "fiveDayRadio";
+            this.fiveDayRadio.Size = new System.Drawing.Size(58, 17);
+            this.fiveDayRadio.TabIndex = 31;
+            this.fiveDayRadio.TabStop = true;
+            this.fiveDayRadio.Text = "5 Days";
+            this.fiveDayRadio.UseVisualStyleBackColor = true;
+            this.fiveDayRadio.Visible = false;
+            // 
+            // threeDayRadio
+            // 
+            this.threeDayRadio.AutoSize = true;
+            this.threeDayRadio.Location = new System.Drawing.Point(340, 255);
+            this.threeDayRadio.Name = "threeDayRadio";
+            this.threeDayRadio.Size = new System.Drawing.Size(58, 17);
+            this.threeDayRadio.TabIndex = 30;
+            this.threeDayRadio.TabStop = true;
+            this.threeDayRadio.Text = "3 Days";
+            this.threeDayRadio.UseVisualStyleBackColor = true;
+            this.threeDayRadio.Visible = false;
+            // 
+            // normalRadio
+            // 
+            this.normalRadio.AutoSize = true;
+            this.normalRadio.Location = new System.Drawing.Point(340, 232);
+            this.normalRadio.Name = "normalRadio";
+            this.normalRadio.Size = new System.Drawing.Size(106, 17);
+            this.normalRadio.TabIndex = 29;
+            this.normalRadio.TabStop = true;
+            this.normalRadio.Text = "Normal (14 Days)";
+            this.normalRadio.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(203, 234);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 13);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "Production Time";
+            this.label5.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(200, 192);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "# of Drawers";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(203, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "Depth";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(203, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Width";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(200, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Surface Material";
+            // 
+            // CreateQuote
+            // 
+            this.CreateQuote.Location = new System.Drawing.Point(271, 373);
+            this.CreateQuote.Name = "CreateQuote";
+            this.CreateQuote.Size = new System.Drawing.Size(88, 35);
+            this.CreateQuote.TabIndex = 23;
+            this.CreateQuote.Text = "Create ";
+            this.CreateQuote.UseVisualStyleBackColor = true;
+            this.CreateQuote.Click += new System.EventHandler(this.CreateQuote_Click);
+            // 
+            // drawerNumberBox
+            // 
+            this.drawerNumberBox.Location = new System.Drawing.Point(340, 185);
+            this.drawerNumberBox.Name = "drawerNumberBox";
+            this.drawerNumberBox.Size = new System.Drawing.Size(121, 20);
+            this.drawerNumberBox.TabIndex = 22;
+            // 
+            // DepthBox
+            // 
+            this.DepthBox.Location = new System.Drawing.Point(340, 147);
+            this.DepthBox.Name = "DepthBox";
+            this.DepthBox.Size = new System.Drawing.Size(121, 20);
+            this.DepthBox.TabIndex = 21;
+            // 
+            // WidthBox
+            // 
+            this.WidthBox.Location = new System.Drawing.Point(340, 113);
+            this.WidthBox.Name = "WidthBox";
+            this.WidthBox.Size = new System.Drawing.Size(121, 20);
+            this.WidthBox.TabIndex = 20;
+            // 
+            // MaterialCombo
+            // 
+            this.MaterialCombo.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.MaterialCombo.FormattingEnabled = true;
+            this.MaterialCombo.Location = new System.Drawing.Point(340, 78);
+            this.MaterialCombo.Name = "MaterialCombo";
+            this.MaterialCombo.Size = new System.Drawing.Size(121, 21);
+            this.MaterialCombo.TabIndex = 19;
+            // 
+            // AddQuote
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.customerNameBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.sevenDayRadio);
+            this.Controls.Add(this.rushButton);
+            this.Controls.Add(this.fiveDayRadio);
+            this.Controls.Add(this.threeDayRadio);
+            this.Controls.Add(this.normalRadio);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CreateQuote);
+            this.Controls.Add(this.drawerNumberBox);
+            this.Controls.Add(this.DepthBox);
+            this.Controls.Add(this.WidthBox);
+            this.Controls.Add(this.MaterialCombo);
+            this.Name = "AddQuote";
+            this.Text = "Add Quote";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CreateQuote_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox customerNameBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RadioButton sevenDayRadio;
+        private System.Windows.Forms.Button rushButton;
+        private System.Windows.Forms.RadioButton fiveDayRadio;
+        private System.Windows.Forms.RadioButton threeDayRadio;
+        private System.Windows.Forms.RadioButton normalRadio;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button CreateQuote;
+        private System.Windows.Forms.TextBox drawerNumberBox;
+        private System.Windows.Forms.TextBox DepthBox;
+        private System.Windows.Forms.TextBox WidthBox;
+        private System.Windows.Forms.ComboBox MaterialCombo;
+    }
+}
